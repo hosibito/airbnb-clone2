6e2b7a1ec0b9092cf88a8575151e27b88b900815
hosibito <hosibito@naver.com>
2021-11-10 23:06:02 +0900

    user app

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+    list_display = UserAdmin.list_display + (
+        "gender",
+        "language",
+        "currency",
+        "superhost",
+    )
+
+
+# 기존 장고 유저 모델을 이용할때.
+# @admin.register(models.User)
+# class CustomUserAdmin(admin.ModelAdmin):
+#     """Custom User Admin"""
+
+#     list_display = ("username", "email", "gender", "language", "currency", "superhost")
+#     list_filter = ("language", "currency", "superhost")
