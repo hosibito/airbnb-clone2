7bf49f8607ef916383d09fbca33ed08892c3454a
hosibito <hosibito@naver.com>
2021-11-18 15:12:52 +0900

    10.0~10.2

--- config/urls.py
+++ config/urls.py
@@ -14,15 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("", include("core.urls", namespace="core")),
     path("admin/", admin.site.urls),
 ]
-
 
 if settings.DEBUG:  # 노트 8-3 참조
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,18 @@
+from datetime import datetime
 from django.shortcuts import render
 
-# Create your views here.
+# from django.http import HttpResponse
+
+
+def all_rooms(request):
+    # print(request)
+    # print(vars(request))
+    # print(dir(request))
+
+    # now = datetime.now()
+    # return HttpResponse(content=f"<h1>{now}</h1>")
+    # return render(request, "all_rooms")
+
+    now = datetime.now()
+    hungry = True
+    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
