22d5dcc29af83fbdb527532fd30cd56c54a93a4b
hosibito <hosibito@naver.com>
2021-11-18 15:58:23 +0900

    10.3~10.4 Templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,18 +1,8 @@
-from datetime import datetime
 from django.shortcuts import render
 
-# from django.http import HttpResponse
+from . import models
 
 
 def all_rooms(request):
-    # print(request)
-    # print(vars(request))
-    # print(dir(request))
-
-    # now = datetime.now()
-    # return HttpResponse(content=f"<h1>{now}</h1>")
-    # return render(request, "all_rooms")
-
-    now = datetime.now()
-    hungry = True
-    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"potato": all_rooms})
