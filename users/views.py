a53e06dae210cf35c04242d8eb9e18e5dc983142
hosibito <hosibito@naver.com>
2021-11-22 20:25:51 +0900

    14.0~14.4 로그인 구현1

--- users/views.py
+++ users/views.py
@@ -7,7 +7,7 @@ from . import forms as user_forms
 
 class LoginView(View):
     def get(self, request):
-        form = user_forms.LoginForm(initial={"email": "test@test.com"})  # 기본값
+        form = user_forms.LoginForm(initial={"email": "test@test.com"})  # 기본 값
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
